3828c3a4041eeab39d5fe10580cee6d92b7abbcb
Martin Sit <46298951+martin226@users.noreply.github.com>
2024-05-26 12:03:31 -0400

    feat: game state model

---
+++ src/model/GameState.java
@@ -0,0 +1,9 @@
+package model;
+
+public class GameState {
+    // Properties
+    public Player player1;
+    public Player player2;
+    public Player currentPlayer;
+    public boolean isGameOver = false; 
+}
