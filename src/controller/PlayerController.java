36f28e7fdee31939913f66eba45ab5df77bc0fb1
Martin Sit <46298951+martin226@users.noreply.github.com>
2024-05-26 12:07:07 -0400

    feat: player controller

---
+++ src/controller/PlayerController.java
@@ -0,0 +1,17 @@
+package controller;
+import model.Player;
+
+public class PlayerController {
+    // Properties
+    private Player player;
+
+    // Methods
+    public void sendMessage(String message) {
+        // TO-DO: Implement sendMessage method
+    }
+
+    // Constructor
+    public PlayerController(Player player) {
+        this.player = player;
+    }
+}
