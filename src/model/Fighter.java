e9e7a1c4f2aa8004fd9169e17b369d59a3827a68
Martin Sit <46298951+martin226@users.noreply.github.com>
2024-05-26 11:58:48 -0400

    feat: fighter model

---
+++ src/model/Fighter.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Fighter {
+    // Properties
+    public int HP = 1000;
+    
+    // Methods
+    public void punch(Fighter opponent) {
+        // TO-DO: Implement attack method
+    }
+
+    public void kick(Fighter opponent) {
+        // TO-DO: Implement attack method
+    }
+
+    public void uppercut(Fighter opponent) {
+        // TO-DO: Implement uppercut method
+    }
+
+    public void block() {
+        // TO-DO: Implement block method
+    }
+
+    public void takeDamage(int damage) {
+        // TO-DO: Implement takeDamage method
+    }
+
+    // Constructor
+    public Fighter() {
+        // TO-DO: Implement constructor
+    }
+
+    public Fighter(int HP) {
+        this.HP = HP;
+    }
+}
