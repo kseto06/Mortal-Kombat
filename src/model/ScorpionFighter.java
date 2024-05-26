741b305835c6c51c49e4e237eaebe217c46218fd
Martin Sit <46298951+martin226@users.noreply.github.com>
2024-05-26 12:01:00 -0400

    feat: scorpion and subzero fighters

---
+++ src/model/ScorpionFighter.java
@@ -0,0 +1,11 @@
+package model;
+
+public class ScorpionFighter extends Fighter {
+    // Properties
+    private long specialLastUsed = 0; // TODO: set to value
+
+    // Methods
+    public void specialMove(Fighter opponent) {
+        // TO-DO: Implement attack method
+    }
+}
