f5e25f22fe88bd3f0dd1ff028cd37427b9416cfb
Kaden司徒逸希 <kaden.seto06@gmail.com>
2024-06-14 13:59:15 -0400

    add: more javadoc comments

--- src/controller/ChatController.java
+++ src/controller/ChatController.java
@@ -2,18 +2,26 @@ package controller;
 import model.GameState;
 import model.Player;
 
+/**ChatController controls the chat logic in the game*/
 public class ChatController {
     // Properties
     private GameState state;
     private Player player;
 
     // Methods
+    /**
+     * sendMessage function sends chat data to SSM to display to the chat screen
+     * @param message Message to be sent
+     */
     public void sendMessage(String message) {
         state.ssm.sendText("chat,"+player.name+","+message);
         state.chat.append(player.name + ": " + message + "\n");
     }
 
-    // Constructor
+    /**
+     * Constructor to initalize the chat controller
+     * @param state Create GameState for SSM functionality here
+     */
     public ChatController(GameState state) {
         this.state = state;
         this.player = state.currentPlayer;
